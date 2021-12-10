3bb6d585c58b2c917a9dc42f958dec95cb1c5f80
mahdi-mirror <93740840+mahdi-mirror@users.noreply.github.com>
2021-12-10 08:56:00 -0800

    cancel heroku
    heroku cancel

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xx#la9b4vne(%f-bt10)ci%i8=@k1+k2#8zrcp8goki8h(w@dp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
